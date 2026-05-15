8d1b0ac5e664b732bc5e32acf98e4f8db585d0b2
jaidenderlam <jaidenderlam@gmail.com>
2026-05-15 16:22:01 -0600

    Update function.js

--- function.js
+++ function.js
@@ -1,121 +1,149 @@
-const c = document.getElementById("gameCanvas");
-const shapes = c.getContext("2d");
-c.width = 700;
-c.height = 500;
+//initial display of the screen
+function drawStart() {
 
-let keyIspressed = false;
+ drawMainStuff();   
+ //Press start to Play
+    shapes.fillStyle = "rgba(37,59,93,0.65)";
+    shapes.fillRect(c.width/4,c.height/4+50, 400,150);
+        // Title
+    shapes.font = "bold 180% Times New Roman";
+    shapes.fillStyle = "#DB9DC2";
+    shapes.fillText("Press play to start!",c.width/4 +75, c.height* 0.50);
 
-//gets the source for image and audios
+   i
+}
+// while the game is operating
+function drawrunGame() {
 
-// inital screen sound
-let initialScreenmusic = document.createElement("audio");
-initialScreenmusic.src ="sound/initialScreen.wav"  ;
-initialScreenmusic.loop = true;
-initialScreenmusic.volume = 0.6;
+    
+    drawPerson();
+    drawMainStuff();
+    drawlandscape();
+    distanceCount();
+}
+//when  the game is over
+function drawgameover() {
+    drawPerson();
+    drawMainStuff();
+    drawlandscape();
+    gameOver();
+    
+ // game over context
 
-let jumpMusic= document.createElement("audio");
-jumpMusic.src ="sound/jumpSound.wav"  ;
-jumpMusic.volume = 0.75;
+shapes.beginPath();
+shapes.lineWidth = 3;
+shapes.ellipse(person.x,person.y,100,100,0,0,Math.Pi *2);
+shapes.strokeStyle = "#C7761A";
+shapes.stroke();
 
-// select your character
-let MaleJoeImg = document.createElement("img");
-MaleJoeImg.src = "img/maleMechJoe.png";
+//game over line
+shapes.fillStyle = "#8E0909";
+shapes.fillText(" Game Over",c.width/4+150,c.height/4+130);
 
-let MaleJoeImgJump = document.createElement("img");
-MaleJoeImgJump.src = "img/maleMechJoeJump.png";
+}
+//draws main components
+function drawMainStuff(){
+    // background heading
+    shapes.fillStyle = "#253B5D";
+    shapes.fillRect(0,0,c.width,80);
+        // Title
+    shapes.font = "bold 180% Times New Roman";
+    shapes.fillStyle = "red";
+    shapes.fillText("The Mechanic Man Game",c.width/2-150, c.height* 0.10);
 
-let character = MaleJoeImg;
+// scores bar
+    shapes.fillStyle = "#253B5D";
+    shapes.fillRect(0,c.height-80,c.width,80);
+ // distance
+    shapes.font = "bold 180% Times New Roman";
+    shapes.fillStyle = "red";
+    shapes.fillText("Distance: " + distances.toFixed(0) + " m",c.width/2 - 250 ,c.height*0.93);
+    shapes.fillText("Best Distance: " + bestDistance.toFixed(0)+ " m", c.width/2 +40, c.height*0.93);
+    
+ // xp bar 
+    shapes.fillStyle = "#253B5D";
+    shapes.fillRect(50,100, c.width *0.20, c.height *0.10);
 
-// power up
+    // points
+    shapes.font = "bold 180% Times New Roman";
+    shapes.fillStyle = "#3C6F5A";
+    shapes.fillText(points + " xp",c.width/4 - 80 ,c.height*0.270);
 
+// health 
+    shapes.drawImage(healthbarImg,healthbar.x,healthbar.y,healthbar.w,healthbar.h);
 
-// health bar status
-let healthbarFull = document.createElement("img");
-healthbarFull.src = "img/healthBar1.png";
-
-let healthbar3quarter = document.createElement("img");
-healthbar3quarter.src = "img/healthBar2.png";
-
-let healthbarhalf = document.createElement("img");
-healthbarhalf.src = "img/healthBar3.png";
-
-let healthbarquarter = document.createElement("img");
-healthbarquarter.src = "img/healthBar4.png";
-
-let healthbarempty = document.createElement("img");
-healthbarempty.src = "img/healthBar5.png";
-
-
-let game;
-let person;
-let pow;
-let healthbar;
-let healthbarImg;
-let landscape;
+    // landscape base
+    shapes.fillStyle = "#535E60";
+    shapes.fillRect(0,c.height-120,c.width,40)
 
 
-window.addEventListener('load',draw);
+}
 
-// defines a function to start game
-function draw() {
-    shapes.clearRect(0,0,c.width,c.height);   
-  
-    if (game === "start") {
-        drawStart();
-        initialScreenmusic.play();
-    } else if (game === "gameison") {
-        drawrunGame();
+game = "start"
+healthbarImg = healthbarFull;
+pow = {
+    x: c.width + 150,
+    y: Math.random()*c.height,
+    w: 20,
+    h:20
+}
+
+healthbar = {
+    x: c.width-120,
+    y: c.height * 0.20 - 10,
+    w: 50,
+    h: 80
+}
+
+person = {
+    x: c.width/4 -90,
+    y: c.height-270,
+    w: c.width/5 +5,
+    h: c.height/5+50 
+}
+
+let distances = 0;
+let bestDistance = 0;
+let points = 0;
+function gameOver () {
+
+    if (bestDistance >= distances) {
+    bestDistance = distances;
+    distances = 0;
+}
+
+}
+
+function distanceCount(){
+    distances += 0.5;
+
+}
+
+function drawPerson() {
+shapes.drawImage(character,person.x,person.y,person.w,person.h);
+jump();
+checkCollision();
+CollectpowerUp();
+}
+
+function checkCollision () {
+    if (person.y <=110){
+        person.y =110;
+    }
+}
+function jump() {
+    if (person.y < c.height-270) {
+        person.y += 5;
+        character = MaleJoeImgJump;
     } else {
-      drawgameover();
+        character = MaleJoeImg;
     }
     
-    //request ANIMATION FRAME
-    requestAnimationFrame(draw);
 }
 
-const StartACar = document.getElementById("StartTheCar");
-
-StartACar.addEventListener('click', function (){
-   startGame();
-   draw();
-
-   document.addEventListener ("keydown",function (event){
-    if(event.key === "ArrowUp"){
-        person.y -= 60;
-
-        keyIspressed = true;
-        jumpMusic.currentTime = 0;
-        jumpMusic.play();
-   
-    } 
-})
-
-document.addEventListener("keyup",mouseUpHandler);
-
-function mouseUpHandler() {
-
-    keyIspressed = false;
-    jumpMusic.pause();
+function CollectpowerUp () {
+     
 }
-
-});
-
-
-function startGame() {
-    //start the initiation of the game
-    if (game === "start") {
-        game = "gameison"
-    }
-
+function drawlandscape() {
+    
 }
-
-
-        
-   
-
-
-
-
-
-
-
